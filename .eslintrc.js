43b3a7c61d23d566d6f466fb4ec563272d3f8718
KhanhVu24 <khanhvuit24.dev@gmail.com>
2022-11-01 23:38:00 +0700

    fix: eslint warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['warn'],
         '@typescript-eslint/no-unused-vars': 'off',
         'no-shadow': 'off',
+        'eslint-disable-next-line': 'off',
+        'react-native/no-inline-styles': 'off',
         'no-undef': 'off',
       },
     },
